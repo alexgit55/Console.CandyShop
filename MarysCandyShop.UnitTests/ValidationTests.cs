bf7e0cb769b20fbaf97c37496f7dfa60991c342e
alexgit55 <121723219+alexgit55@users.noreply.github.com>
2024-12-11 13:42:12 -0500

    Created string validation tests

--- MarysCandyShop.UnitTests/ValidationTests.cs
+++ MarysCandyShop.UnitTests/ValidationTests.cs
@@ -7,6 +7,17 @@ namespace MarysCandyShop.UnitTests
         {
             var stringInput = "Test Chocolate Bar";
             var result = Validation.IsStringValid(stringInput);
+
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("This is a string with more than 20 characters")]
+        public void WhenStringIsNotValidReturnFalse(string input)
+        {
+            var result = Validation.IsStringValid(input);
+            Assert.False(result);
         }
     }
 }
