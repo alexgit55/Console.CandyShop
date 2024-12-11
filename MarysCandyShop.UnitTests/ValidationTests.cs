2dbc6c3d3356fec32fe16f10b080d7b8835d66b1
alexgit55 <121723219+alexgit55@users.noreply.github.com>
2024-12-11 16:04:33 -0500

    Added comments to unit test methods

--- MarysCandyShop.UnitTests/ValidationTests.cs
+++ MarysCandyShop.UnitTests/ValidationTests.cs
@@ -2,6 +2,8 @@ namespace MarysCandyShop.UnitTests
 {
     public class ValidationTests
     {
+
+        // Test the IsStringValid method, which should return true if the string is not null or empty and has a length of 20 characters or less.
         [Fact]
         public void WhenStringIsValidReturnTrue()
         {
@@ -11,6 +13,7 @@ namespace MarysCandyShop.UnitTests
             Assert.True(result);
         }
 
+        // Test the IsStringValid method, which should return false if the string is null, empty, or has a length of more than 20 characters.
         [Theory]
         [InlineData("")]
         [InlineData("This is a string with more than 20 characters")]
@@ -20,6 +23,7 @@ namespace MarysCandyShop.UnitTests
             Assert.False(result);
         }
 
+        // Test the IsPriceValid method, which should return true if the price is a valid number between 0 and 9999.
         [Theory]
         [InlineData("20")]
         [InlineData("20.5")]
@@ -29,6 +33,8 @@ namespace MarysCandyShop.UnitTests
             Assert.True(result.IsValid);
         }
 
+
+        // Test the IsPriceValid method, which should return false if the price is not a valid number or is outside the range of 0 to 9999.
         [Theory]
         [InlineData("")]
         [InlineData("Not a number")]
@@ -40,6 +46,8 @@ namespace MarysCandyShop.UnitTests
             Assert.False(result.IsValid);
         }
 
+
+        // Test the IsCocoaPercentageValid method, which should return true if the cocoa percentage is a valid number between 0 and 99.
         [Theory]
         [InlineData("10")]
         [InlineData("50")]
@@ -49,6 +57,8 @@ namespace MarysCandyShop.UnitTests
             Assert.True(result.IsValid);
         }
 
+
+        // Test the IsCocoaPercentageValid method, which should return false if the cocoa percentage is not a valid number or is outside the range of 0 to 99.
         [Theory]
         [InlineData("")]
         [InlineData("Not a number")]
